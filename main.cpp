98393d099805ae1b6ef5b862ecac80ce5db154c5
Mikhail Buinouski <m.buynovsky@syberry.com>
2023-05-06 14:11:07 +0300

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cstdlib>
+
+int main(int argc, char **argv) {
+
+    std::cout << "Hello world!" << std::endl;
+
+    std::cout << "Test workflow 2" << std::endl;
+
+    return EXIT_SUCCESS;
+}
