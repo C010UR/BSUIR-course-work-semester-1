e26c78bd344669f96d3127a0f43f29558a6c2b56
C010UR <C010UR@users.noreply.github.com>
2023-05-06 15:11:57 +0000

    [Github Actions] Lint with clang-format

--- src/main.cpp
+++ src/main.cpp
@@ -65,31 +65,30 @@ Grid make_diagram1() {
     return grid;
 }
 
-template<typename Location, typename Graph>
-std::unordered_map<Location, Location>
-breadth_first_search(Graph graph, Location start, Location goal) {
-  std::queue<Location> frontier;
-  frontier.push(start);
+template <typename Location, typename Graph>
+std::unordered_map<Location, Location> breadth_first_search(Graph graph, Location start, Location goal) {
+    std::queue<Location> frontier;
+    frontier.push(start);
 
-  std::unordered_map<Location, Location> came_from;
-  came_from[start] = start;
+    std::unordered_map<Location, Location> came_from;
+    came_from[start] = start;
 
-  while (!frontier.empty()) {
-    Location current = frontier.front();
-    frontier.pop();
+    while (!frontier.empty()) {
+        Location current = frontier.front();
+        frontier.pop();
 
-    if (current == goal) {
-        break;
+        if (current == goal) {
+            break;
+        }
+
+        for (Location next : graph.neighbors(current)) {
+            if (came_from.find(next) == came_from.end()) {
+                frontier.push(next);
+                came_from[next] = current;
+            }
+        }
     }
-
-    for (Location next : graph.neighbors(current)) {
-      if (came_from.find(next) == came_from.end()) {
-        frontier.push(next);
-        came_from[next] = current;
-      }
-    }
-  }
-  return came_from;
+    return came_from;
 }
 
 int main(void) {
